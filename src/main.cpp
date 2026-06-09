24091a6fe16d83275a4e05587e30deb75cd45166
elijahzhao24 <elijahzhao24@gmail.com>
2026-06-08 21:13:32 -0700

    Add smoke tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,33 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+constexpr unsigned long HEARTBEAT_INTERVAL_MS = 1000;
+unsigned long lastHeartbeatMs = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  Serial.setTimeout(50);
+
+  delay(1000);
+  Serial.println();
+  Serial.println("BizBot ESP32 firmware started");
+  Serial.println("Type a line and press Enter to test USB serial.");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  if (Serial.available() > 0) {
+    String line = Serial.readStringUntil('\n');
+    line.trim();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    if (!line.isEmpty()) {
+      Serial.print("ESP32 received: ");
+      Serial.println(line);
+    }
+  }
+
+  const unsigned long now = millis();
+  if (now - lastHeartbeatMs >= HEARTBEAT_INTERVAL_MS) {
+    lastHeartbeatMs = now;
+    Serial.print("HEARTBEAT uptime_ms=");
+    Serial.println(now);
+  }
 }
